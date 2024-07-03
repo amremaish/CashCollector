4e518cdd878750a3430f9c58ea1dd87dd5d43363
amrmTSN <amrm@trustsecurenow.com>
2024-07-04 00:50:45 +0300

    add timestamp for models

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from apps.core.models import TimeStampedModel
 
-class User(AbstractUser):
+
+class User(AbstractUser, TimeStampedModel):
     is_cash_collector = models.BooleanField(default=False)
     is_manager = models.BooleanField(default=False)
     is_frozen = models.BooleanField(default=False)
 
 
-class Customer(models.Model):
+class Customer(TimeStampedModel):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
